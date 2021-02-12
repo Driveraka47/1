e772d9bc85586200065985efb84c1b1263ec55eb
Vladislav <stosoarer@gmail.com>
2021-02-12 13:54:41 +0300

    Last Home Work

---
+++ HW/Les4/MethodsForArrays.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+
+namespace HomeWork4._2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+         /*Это решение нашел на сайте, смысл в общем понял, 
+          * но в целом работа с массивами для меня кажется уже сложной и
+          придерживатся того же принципа что и раньше, писать весь код самостоятельно,
+         я уже не могу. Видимо придется перепроходить курс с начала.*/
+        
+        
+        }
+        public class Arrays
+        {
+            public int[] MainArray { get; set; }
+            public int Maxcout { get; set; }
+
+            public Arrays(int count, int start, int step)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    MainArray[i] = start + (step * i);
+                }
+            }
+
+            public int Sum(int[] arr)
+            {
+                int s = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    s += arr[i];
+                }
+                return s;
+            }
+
+            public int[] Inverse(int[] arr)
+            {
+                int[] newArr = new int[arr.Length];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    newArr[i] = -arr[i];
+                }
+
+                return newArr;
+            }
+
+            public int[] Multi(int[] arr, int mlt)
+            {
+                int[] newArr = new int[arr.Length];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    newArr[i] = mlt * arr[i];
+                }
+
+                return newArr;
+            }
+
+            public int MaxCounter(int[] arr)
+            {
+                int max = arr.Max();
+                int s = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == max)
+                        s += 1;
+                }
+                return s;
+            }
+
+        }
+    }
+
+}    
+    
+    
+
+
+
+
+    
+    
+       
+        
+
+
+
+
+       
+
+
+    
---
+++ HW/Les4/RandomArray.cs
@@ -0,0 +1,60 @@
+﻿#region Автор Ткаченко Влад
+#endregion
+/*Дан целочисленный массив из 20 элементов. Элементы массива могут принимать целые значения от –10 000 до 10 000 включительно.
+ * Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых хотя бы одно число делится на 3.
+ * В данной задаче под парой подразумевается два подряд идущих элемента массива.
+ * Например, для массива из пяти элементов: 6; 2; 9; –3; 6 – ответ: 4.*/
+using System;
+
+namespace HomeWorke4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myMassive myArray = new myMassive(20, -10000, 10000);
+            Console.WriteLine(myArray.ToString());
+            myArray.Divisibility3();
+            Console.ReadLine();
+        }
+
+        public class myMassive
+        {
+            private int[] am;
+          
+            public myMassive(int n, int min, int max)
+            {
+                am = new int[n];
+                Random random = new Random();
+                for (int i = 0; i < n; i++)
+                    am[i] = random.Next(min, max);
+            }
+
+            
+            public int Divisibility3()
+            {
+                
+                    int count = 0;
+                    for (int i = 0; i < am.Length - 1; i++)
+                    {
+                        if (am[i] % 3 == 0 || am[i + 1] % 3 == 0)
+                            count++;
+                        Console.WriteLine("Пара чисел: {0} и {1}", am[i], am[i + 1]);
+                    }
+                    Console.WriteLine("Количество пар: " + count);
+                    return count;
+                
+            }
+
+           
+            override public string ToString()
+            {
+                string stringarray = "";
+                foreach (int x in am)
+                    stringarray = stringarray + x + " ";
+                return stringarray;
+            }
+        }
+    }
+    
+}
